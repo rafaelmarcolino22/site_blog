a905b05a4e7d7e692abe1637dbd76d012c2549ea
rafaelmarcolino22 <ralfrr55@gmail.com>
2020-11-13 15:54:59 -0300

    .

---
+++ site_root/postblog/models.py
@@ -0,0 +1,15 @@
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.auth.models import User
+from django.db import models
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=256)
+    conteudo = RichTextField()
+    resumo = RichTextUploadingField()
+    autor = models.ForeignKey(User ,on_delete=models.PROTECT)
+    createad_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+# Create your models here.
