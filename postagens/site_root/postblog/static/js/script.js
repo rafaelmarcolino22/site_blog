700763e7244a8c7755bcc8540007a520e71f7842
rafaelmarcolino22 <ralfrr55@gmail.com>
2020-11-20 18:19:54 -0300

    .

--- postagens/site_root/postblog/static/js/script.js
+++ postagens/site_root/postblog/static/js/script.js
@@ -1,11 +1,26 @@
 
 function entrar() {
     document.getElementById('divRight').style.color = 'blue'
-
     document.getElementById('divRight').style.background = 'rgb(165, 164, 164)'
 }
       
 function sair() {
     document.getElementById('divRight').style.background = 'white'
+}
 
+function leftEnter(){
+    var sei = document.getElementById('imd_brinde')
+    sei.innerHTML = 'Click AQUI  -- E SE  -- CADASTRE'
+    sei.style.color = 'red'
+    sei.style.fontSize = '60px'
+}
+
+function leftSair(){
+    
+    var sei = document.getElementById('imd_brinde')
+    sei.innerHTML = 'Quer ganhar   um Brinde'
+    sei.style.color = 'white'
+    sei.style.fontSize = '90px'
+    sei.style.textAlign = 'center';
+    
 }
